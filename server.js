8722ab67709bd21e6ef1809892f3854847b013b2
Gabriel Isaac <iamkemical1@gmail.com>
2024-10-27 20:10:13 +0100

    Changed from PORT to APP_PORT

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const twilio = require("twilio");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.APP_PORT || 5000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
